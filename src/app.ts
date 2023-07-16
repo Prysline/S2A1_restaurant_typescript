c26d882ca3923f63b7a383605d7a00739f5840a2
DESKTOP-MCU8D69\User <mayhu1993@gmail.com>
2023-07-16 23:11:26 +0800

    func: add express-handlebars

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import * as handlebars from "express-handlebars";
 
 dotenv.config({ path: "env/.env." + process.env.NODE_ENV });
 
 const app: Express = express();
 const port = process.env.PORT || 5001;
 const host: String = process.env.HOST || "localhost";
+const hbs: handlebars.ExpressHandlebars = handlebars.create({ defaultLayout: "main" });
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.use(express.static("public"));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.render("index", { user: { name: "test" } });
 });
 
 app.listen(port, () => {
