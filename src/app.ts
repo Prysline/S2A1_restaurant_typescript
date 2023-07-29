993b2be70877b2db31a13085a78516b3319312a4
prysline <mayhu1993@gmail.com>
2023-07-29 14:17:58 +0800

    fix: do use type string

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: "env/.env." + process.env.NODE_ENV });
 
 const app: Express = express();
 const port = process.env.PORT || 5001;
-const host: String = process.env.HOST || "localhost";
+const host: string = process.env.HOST || "localhost";
 const hbs: handlebars.ExpressHandlebars = handlebars.create({ defaultLayout: "main" });
 
 app.engine("hbs", hbs.engine);
@@ -15,10 +15,6 @@ app.set("view engine", "hbs");
 app.set("views", "src/views");
 
 app.use(express.static("public"));
-
-app.get("/", (req: Request, res: Response) => {
-  res.render("index", { user: { name: "test" } });
-});
 
 app.use(routes);
 
